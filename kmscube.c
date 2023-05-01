015bbe1bf158e4f9ad95d338edda03aa84df67de
jamescheng40 <cheng-james@hotmail.com>
2023-05-01 06:13:39 -0400

    Revert "creating loopempty condition for display static framebuffer"
    This reverts commit 28d7ab07f1a3695467f200ee0c4109749900f0ef.

--- kmscube.c
+++ kmscube.c
@@ -831,74 +831,63 @@ int main(int argc, char *argv[])
 			return ret;
 		}
 	}
-	bool loopempty = false;
-	while (loopempty == false) {
 
-		if(loopempty == true)
-		{
-			
-		}
-		else
-		{
-			struct gbm_bo *next_bo;
-			int waiting_for_flip;
-			int cc;
+	while (frame_count != 0) {
+		struct gbm_bo *next_bo;
+		int waiting_for_flip;
+		int cc;
 
-			//draw(i++);
+		draw(i++);
 
-			eglSwapBuffers(gl.display, gl.surface);
-			next_bo = gbm_surface_lock_front_buffer(gbm.surface);
-			fb = drm_fb_get_from_bo(next_bo);
+		eglSwapBuffers(gl.display, gl.surface);
+		next_bo = gbm_surface_lock_front_buffer(gbm.surface);
+		fb = drm_fb_get_from_bo(next_bo);
 
-			/*
-			* Here you could also update drm plane layers if you want
-			* hw composition
-			*/
+		/*
+		 * Here you could also update drm plane layers if you want
+		 * hw composition
+		 */
 
-			if (all_display) {
-				for (cc=0;cc<drm.ndisp; cc++) {
-					ret = drmModePageFlip(drm.fd, drm.crtc_id[cc], fb->fb_id,
-						DRM_MODE_PAGE_FLIP_EVENT, &waiting_for_flip);
-					if (ret) {
-						printf("failed to queue page flip: %s\n", strerror(errno));
-						return -1;
-					}
-				}
-				waiting_for_flip = drm.ndisp;
-			} else {
-				ret = drmModePageFlip(drm.fd, drm.crtc_id[DISP_ID], fb->fb_id,
-						DRM_MODE_PAGE_FLIP_EVENT, &waiting_for_flip);
+		if (all_display) {
+			for (cc=0;cc<drm.ndisp; cc++) {
+				ret = drmModePageFlip(drm.fd, drm.crtc_id[cc], fb->fb_id,
+					DRM_MODE_PAGE_FLIP_EVENT, &waiting_for_flip);
 				if (ret) {
 					printf("failed to queue page flip: %s\n", strerror(errno));
 					return -1;
 				}
-				waiting_for_flip = 1;
 			}
-
-			while (waiting_for_flip) {
-				ret = select(drm.fd + 1, &fds, NULL, NULL, NULL);
-				if (ret < 0) {
-					printf("select err: %s\n", strerror(errno));
-					return ret;
-				} else if (ret == 0) {
-					printf("select timeout!\n");
-					return -1;
-				} else if (FD_ISSET(0, &fds)) {
-					continue;
-				}
-				drmHandleEvent(drm.fd, &evctx);
+			waiting_for_flip = drm.ndisp;
+		} else {
+			ret = drmModePageFlip(drm.fd, drm.crtc_id[DISP_ID], fb->fb_id,
+					DRM_MODE_PAGE_FLIP_EVENT, &waiting_for_flip);
+			if (ret) {
+				printf("failed to queue page flip: %s\n", strerror(errno));
+				return -1;
 			}
-
-			/* release last buffer to render on again: */
-			gbm_surface_release_buffer(gbm.surface, bo);
-			bo = next_bo;
-
-					if(frame_count >= 0)
-					frame_count--;
-
-			loopempty = true;
+			waiting_for_flip = 1;
 		}
 
+		while (waiting_for_flip) {
+			ret = select(drm.fd + 1, &fds, NULL, NULL, NULL);
+			if (ret < 0) {
+				printf("select err: %s\n", strerror(errno));
+				return ret;
+			} else if (ret == 0) {
+				printf("select timeout!\n");
+				return -1;
+			} else if (FD_ISSET(0, &fds)) {
+				continue;
+			}
+			drmHandleEvent(drm.fd, &evctx);
+		}
+
+		/* release last buffer to render on again: */
+		gbm_surface_release_buffer(gbm.surface, bo);
+		bo = next_bo;
+
+                if(frame_count >= 0)
+		        frame_count--;
 	}
 
 	cleanup_kmscube();

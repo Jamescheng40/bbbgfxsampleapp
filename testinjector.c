071f91a05f2684b72cb1a2b272c621696a3cb872
jamescheng40 <cheng-james@hotmail.com>
2023-05-02 03:16:08 -0400

    [INjector] adding testinjector for testing some fns

---
+++ testinjector.c
@@ -0,0 +1,30 @@
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <dlfcn.h>
+#include <stdint.h>
+#include <link.h>
+
+static void* real_dlopen(const char* filename, int flags)
+{
+
+    void* ret = dlsym(RTLD_NEXT, "dlopen");
+    if (ret == NULL) {
+        fprintf(stderr, "could not dlsym %s\n", "dlopen");
+    }
+
+    return ret;
+
+}
+
+void* dlopen(const char* filename, int flags)
+{
+    void* ret = real_dlopen(filename, flags | RTLD_GLOBAL);
+    if (ret) {
+        printf("\ndlopened: %s\n", filename);
+    }
+
+    dl_iterate_phdr(iterate_callback, 0);
+    return ret;
+
+}

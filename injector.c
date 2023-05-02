fcc540d486e8cdebe68be7b39348a0f0e914ebea
jamescheng40 <cheng-james@hotmail.com>
2023-05-02 03:57:41 +0000

    placebo

--- injector.c
+++ injector.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dlfcn.h>
 
 static void* (*real_malloc)(size_t size);
 
 void* malloc(size_t size) {
         if (!real_malloc) {
+	    printf("ok we it fnc inside if \n");
             real_malloc = dlsym(RTLD_NEXT, "malloc");
             //check if the pointer is valid
         }

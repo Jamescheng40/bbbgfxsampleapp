34a8e1021babc54f048576a4ee8fe6b2735c9893
jamescheng40 <cheng-james@hotmail.com>
2023-05-02 07:19:09 +0000

    Revert "[INjector] fix compile bug"
    This reverts commit a92e7c3499d0b12dbcd3ec206e259f6223edc7cf.

--- testinjector.c
+++ testinjector.c
@@ -5,26 +5,6 @@
 #include <stdint.h>
 #include <link.h>
 
-static int iterate_callback (struct dl_phdr_info *info, size_t sze, void * ptr)  {
-    size_t last_addr = 0;
-
-    for (int i = 0; i < info->dlpi_phnum; i++) {
-        ElfW(Phdr) cur = info->dlpi_phdr[i];
-        uintptr_t  first = info->dlpi_addr + cur.p_vaddr;
-        uintptr_t  last  = first + cur.p_memsz - 1;
-
-        if (last > last_addr) {
-            last_addr = last;
-        }
-    }
-    Library new = { .base = info->dlpi_addr, .size = (last_addr - info->dlpi_addr), .name = (info->dlpi_name) };
-
-    //TODO reduce memory waste with a hashmap to avoid having the same library multiple times since they are all listed upon dlopen
-    arrput(libraries, new);
-
-    return 0;
-}
-
 static void* real_dlopen(const char* filename, int flags)
 {
 
